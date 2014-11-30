efa1c3daad38d2e8b7833a6d2f001850dbd34284
Attila Varga <artit91@gmail.com>
2014-11-30 20:42:06 +0100

    openFileDirective refactor

--- app/js/directives/openFileDirective.js
+++ app/js/directives/openFileDirective.js
@@ -1,15 +1,25 @@
-amplitude.directive("openFile", function($parse) {
-	return {
-	    restrict: 'A',
-	    scope: { 
-	      model: "=ngModel"
-	    },
-	    link: function($scope, $element, $attributes) {
-	    	$element.on("change", function() {
-		  		$scope.model.parse($element[0].files, function() {
-		    		$element.val(null);
-		  		});
-	    	});
-	   	}
-	}
-});
+
+/*jslint browser: true*/
+/*global angular, amplitude*/
+
+(function () {
+
+    'use strict';
+
+    amplitude.directive("openFile", function () {
+        return {
+            restrict: 'A',
+            scope: {
+                model: "=ngModel"
+            },
+            link: function ($scope, $element) {
+                $element.on("change", function () {
+                    $scope.model.parse($element[0].files, function () {
+                        $element.val(null);
+                    });
+                });
+            }
+        };
+    });
+
+}());
